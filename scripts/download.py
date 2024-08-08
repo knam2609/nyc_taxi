11dd0fcc0cc56802e6361da7c2189182703b3c93
knam2609 <167292207+knam2609@users.noreply.github.com>
2024-08-08 18:40:33 +1000

    Create every directories

--- scripts/download.py
+++ scripts/download.py
@@ -5,15 +5,14 @@ from typing import List
 import os
 print(os.getcwd())  # This will show you the current directory from where the notebook is running
 
-def make_directories(output_relative_dir: str, target_dirs: List[str]):
+def make_directories(output_relative_dirs: List[str], target_dirs: List[str]):
     """ Create directories for data """
-    if not os.path.exists(output_relative_dir): # check if directories already exist
-        os.makedirs(output_relative_dir)
-    
-
-    for td in target_dirs:
-        if not os.path.exists(output_relative_dir + td): # check if directories already exist
-            os.makedirs(output_relative_dir + td)
+    for ord in output_relative_dirs:
+        if not os.path.exists(ord): # check if directories already exist
+            os.makedirs(ord)
+        for td in target_dirs:
+            if not os.path.exists(ord + td): # check if directories already exist
+                os.makedirs(ord + td)
 
 def download_files(url_template: str, output_dir: str, years: List[int], months: List[int]):
     """ Download data from url """

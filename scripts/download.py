bf0bbdc72ca806d1a22be6ec4248bcc9d34cc361
knam2609 <167292207+knam2609@users.noreply.github.com>
2024-08-15 17:04:45 +1000

    download data

--- scripts/download.py
+++ scripts/download.py
@@ -2,9 +2,6 @@ import os
 from urllib.request import urlretrieve
 from typing import List
 
-import os
-print(os.getcwd())  # This will show you the current directory from where the notebook is running
-
 def make_directories(output_relative_dirs: List[str], target_dirs: List[str]):
     """ Create directories for data """
     for ord in output_relative_dirs:
@@ -14,18 +11,17 @@ def make_directories(output_relative_dirs: List[str], target_dirs: List[str]):
             if not os.path.exists(ord + td): # check if directories already exist
                 os.makedirs(ord + td)
 
-def download_files(url_template: str, output_dir: str, years: List[int], months: List[int]):
+def download_files(url_template: str, output_dir: str, year: int, months: List[int]):
     """ Download data from url """
-    for year in years:
-        for month in months:
-            month = str(month).zfill(2) 
-            print(f"Begin month {month}")
-        
-            # generate url
-            url = f'{url_template}{year}-{month}.parquet'
-            # generate output location and filename
-            output_dir = f"{output_dir}/{year}-{month}.parquet"
-            # download
-            urlretrieve(url, output_dir) 
+    for month in months:
+        month = str(month).zfill(2)
+        print(f"Begin month {month}")
+        # generate url
+        url = f'{url_template}{year}-{month}.parquet'
+        print(url)
+        # generate output location and filename
+        output_file = f"{output_dir}/{year}-{month}.parquet"
+        # download
+        urlretrieve(url, output_file)
             
-            print(f"Completed month {month}")
+        print(f"Completed month {month}")
